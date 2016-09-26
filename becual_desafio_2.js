af890f26f049c1423239a0eb4d55436d3b27637f
moneirao <moises.neira.9@gmail.com>
2016-09-26 05:28:46 -0300

    Update becual_desafio_2.js

--- becual_desafio_2.js
+++ becual_desafio_2.js
@@ -24,6 +24,7 @@ function imprimir_array_output(array) {
 }
 
 var arreglo_input = [5,4,2,7,9,3,8];
+var d = 2;
 
-rotadorArray(arreglo_input, 2);
+rotadorArray(arreglo_input, d);
 imprimir_array_output(arreglo_input);
