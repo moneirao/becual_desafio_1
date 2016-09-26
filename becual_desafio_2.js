71b34ff616b2cf9cdcc01f8e0e4925ce777b2387
moneirao <moises.neira.9@gmail.com>
2016-09-26 05:25:25 -0300

    Add files via upload
    Desarrollo del Desafio Becual #2

---
+++ becual_desafio_2.js
@@ -0,0 +1,29 @@
+function rotadorArrayDeUno(array) {
+	var i, aux; 
+
+	aux = array[0];
+	for (i = 0; i < array.length - 1; i++) {
+		array[i] = array[i + 1];
+	}
+	array[i] = aux;
+}
+
+function rotadorArray(array, d) {
+	var i;
+	for (i = 0; i < d; i++) {
+		rotadorArrayDeUno(array);
+	}
+}
+
+function imprimir_array_output(array) {
+	var i;
+
+	for (i = 0; i < array.length; i++) {
+		console.log(array[i] + ' ');
+	}
+}
+
+var arreglo_input = [5,4,2,7,9,3,8];
+
+rotadorArray(arreglo_input, 2);
+imprimir_array_output(arreglo_input);
